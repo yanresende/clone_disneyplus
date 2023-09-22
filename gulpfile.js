34e0089039d697848fa27e182f62c435a34d5825
Yan Araujo <saresende555@gmail.com>
2023-09-22 14:08:42 -0300

    i

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ function styles() {
 }
 
 function images() {
-    return gulp.src('./src/images/**/*')
+    return gulp.src('./src/Images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
